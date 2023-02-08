6e739e1872a3fe5efffa2a23eddcab9a801231fc
neelinderbir <122473511+neelinderbir@users.noreply.github.com>
2023-02-08 13:37:47 -0500

    Create sysinfo.sh

---
+++ bash/sysinfo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+hello world
+today me neel goin to show u my script
+echo "here is the fully qualified domain is:"
+hostname
+echo "Here is the host information:"
+hostnamectl
+echo "IP address of this server  is :"
+hostname -I
+echo "Spcae available in the root system:"\
+df -h/
+
+
+hope u like my script see yaa.
